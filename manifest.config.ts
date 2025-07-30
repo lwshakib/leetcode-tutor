d57017153a82b9eecafd6a8bcf2c2dc3675660f2
lwshakib <leadwithshakib@gmail.com>
2025-07-30 17:52:37 +0600

    RELEASE 1.0

---
+++ manifest.config.ts
@@ -0,0 +1,27 @@
+import { defineManifest } from '@crxjs/vite-plugin'
+import pkg from './package.json'
+
+export default defineManifest({
+  manifest_version: 3,
+  name: pkg.name,
+  version: pkg.version,
+  icons: {
+    16: 'public/icon16.png',
+    32: 'public/icon32.png',
+    48: 'public/icon48.png',
+    128: 'public/icon128.png'
+  },
+  action: {
+    default_icon: {
+      16: 'public/icon16.png',
+      32: 'public/icon32.png',
+      48: 'public/icon48.png',
+      128: 'public/icon128.png'
+    },
+    default_popup: 'src/popup/index.html',
+  },
+  content_scripts: [{
+    js: ['src/content/main.tsx'],
+    matches: ['https://*/*'],
+  }],
+})
